f3af78413c83a9c5f42499c9e6f1b374be647016
Kavinkumar2505 <130956673+Kavinkumar2505@users.noreply.github.com>
2023-08-20 06:10:11 +0000

    added files

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+Jobs = [{
+  'id': 1,
+  'title': 'Data Scientist',
+  'Location': 'Bengalore',
+  'Salary': '1,00,000'
+}, {
+  'id': 2,
+  'title': 'Data Analyst',
+  'Location': 'Chennai',
+  'Salary': '50,000'
+}, {
+  'id': 3,
+  'title': 'Full-Stack Developer',
+  'Location': 'Delhi',
+  'Salary': '70,000'
+}, {
+  'id': 4,
+  'title': 'Frontend Developer',
+  'Location': 'Pune',
+  'Salary': '35000'
+}]
 
 
 @app.route("/")
 def home():
-  return "kavin"
+  return render_template("home.html", jobs=Jobs)
 
 
 if __name__ == "__main__":
